90e21036f290bbd77991aa2a2a94be122ab02829
lcky <lcky>
2025-02-23 00:14:10 +0100

    chrome optIONS

--- validators/keySchema/keyschema.py
+++ validators/keySchema/keyschema.py
@@ -58,7 +58,7 @@ class keySchema(Schema):
     @validates('amount')
     def validate_amount(self, value):
         if value <= 500:
-            raise ValidationError("Amount must be greater than 0.")
+            raise ValidationError("Amount must be greater than 500.")
 
     @validates('trading_view_login')
     def validate_trading_view_login(self, value):
